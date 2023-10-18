1baf1ea101f51b1b58e6d0e4a7f89ea5e3df8da6
krs <kristianrorensvanholm@gmail.com>
2023-10-18 13:07:00 +0200

    Initial commit

---
+++ background_scripts/background.js
@@ -0,0 +1,119 @@
+const apiBTN = document.getElementById("insert")
+const sortBTN = document.getElementById("btn")
+const apiDIV = document.getElementById("API_TOKEN")
+const tabCount = document.getElementById("tabCount")
+
+sortBTN.addEventListener('click', sort)
+apiBTN.addEventListener('click', addKEY)
+
+let API_KEY = localStorage.getItem("key")
+if (API_KEY != null && API_KEY != "null" && validKEY(API_KEY)) {
+	console.log("API KEY FOUND")
+	sortBTN.style.display = "block"
+	apiDIV.style.display = "none"
+} else {
+	console.log("NO API KEY!")
+	sortBTN.style.display = "none"
+	apiDIV.style.display = "block"
+}
+
+
+async function addKEY(){
+	const textbox = document.getElementById("api_key_field")
+	console.log(textbox.value)
+	const valid = await validKEY(textbox.value)
+
+	if(valid) {
+		localStorage.setItem("key", textbox.value)
+		API_KEY = textbox.value
+		sortBTN.style.display = "block"
+		apiDIV.style.display = "none"
+	}
+	else console.log("INVALID!")
+}
+
+async function validKEY(key){
+	const url = `https://www.googleapis.com/youtube/v3/videos?id=dQw4w9WgXcQ&part=contentDetails&key=${key}`
+	const data = await fetch(url)
+	return data.status == 200
+}
+
+async function fetchData(tabs) {
+    const numRuns = (tabs.length % 50) + 1 // Something im misunderstanding here lol. Gives 8 runs for 57 which should give one run.
+
+    const items = []
+    for(let i = 0; i < numRuns; i++){
+        const start = i*50
+        const stop = Math.min((i+1)*50, tabs.length)
+        
+        // numRuns were innaccurate
+        if (start > stop) {
+            break
+        }
+    
+        // Actually fetching the timestamps 
+        const currIDS = tabs.slice(start, stop)
+        const url = `https://www.googleapis.com/youtube/v3/videos?id=${currIDS.toString()}&part=contentDetails&key=${API_KEY}`
+        const result = await fetch(url).then((data) => data.json()).then((json) => json.items).catch((e) => {tabCount.innerText="BORKED"+e; return})
+        
+        // Adding to list
+        items.push.apply(items, result)
+    }
+
+    return items
+}
+
+async function sort(){
+  	console.log("SORTING...")
+	let tabs = await browser.tabs.query({currentWindow:true,url: "https://www.youtube.com/watch?v=*"}).then(result => result)
+
+	if(tabs.length==0) return // Stop.
+
+	// Get all IDS
+	let ids=[]
+	for(const tab of tabs){
+		ids.push(tab.url.substr(32,11)) // 32 Chars until video ID
+	}
+
+	// Fetch durations
+    const items = await fetchData(ids)
+	        
+	// Append durations to tab list 
+	for(let i = 0; i < items.length; i++){
+		tabs[i].duration = parseDuration(items[i].contentDetails.duration)
+	}
+
+	// Sort and move tabs
+	tabs.sort(compareDuration)
+	browser.tabs.move(
+		tabs.map((tab) => {return tab.id;}),
+		{ index: 1}
+	)
+
+    tabCount.innerText = tabs.length
+}
+
+// Convert "PT18M6S" to 1086 (seconds) ISO-8601
+// Pretty sure there are a bunch of edge cases not handled here
+function parseDuration(charset){
+	const numberPattern = /\d+/g;
+	const nums = charset.match(numberPattern)
+
+	return calculateDuration(nums, 1, nums.length)
+}
+
+// Calculate duration in seconds. Flexible between 1 and 3 digits in arr.
+function calculateDuration(arr, count, total){
+	if (count == total+1) return 0
+	return arr[count-1] * Math.pow(60, total-count) + calculateDuration(arr, count+1, total)
+}
+
+function compareDuration(a,b){
+	if (b.duration > a.duration) {
+		return -1;
+	} else if (b.duration < a.duration) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Youtube Sorter
+Do you watch way too much YouTube during your free / gaming / working hours?  
+If as me, your answer to that question is an astounding "yes, its most likely an addiction at this point", this extension might be what you need to further fuel your habits!  
+YTSort allows for sorting any amount of YouTube videos on their duration which works really well with what I call the "tab-queue" of videos.  
+
+By middle clicking each video you want to watch and utilizing YTSort, you can quickly get a sorted list of videos to watch from shortest to longest.
+
+## Install
+Grab the newest release from [here](https://github.com/KristianSvanholm/YTSort/releases/)!  
+If on firefox, simply clicking the release download should automatically install it, otherwise you will need to run it.
+
+## API TOKEN
+Create project at google Cloud and create API token with access to YouTube API V3 (public api)
+
+Use that token when prompted for it in extension popup.
+
+## Sort
+Click "sort" in the extension to sort all open YouTube tabs in order of duration.
